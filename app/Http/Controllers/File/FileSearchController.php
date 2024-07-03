76685e6c4317330ce8817ad577c4125f8ba4543c
DucTran1605 <snowhead2003@gmail.com>
2024-07-03 10:08:14 +0700

    feat: Add search file in trash page

--- app/Http/Controllers/File/FileSearchController.php
+++ app/Http/Controllers/File/FileSearchController.php
@@ -34,4 +34,30 @@ class FileSearchController extends Controller
 
         return view('layouts.home.main_page', compact('files'));
     }
+
+    /**
+     * Search file by File Name
+     *
+     * @param [type] $filename
+     * @return void
+     */
+    public function fileTrashSearch(Request $request, $folder_id = null)
+    {
+        $fileNameSearch = $request->file_search;
+
+        if ($fileNameSearch == null) {
+            $files = File::onlyTrashed()->where([
+                ['parent_id', '=', $folder_id],
+                ['user_id', '=', auth()->id()],
+            ])->get();
+        } else {
+            $files = File::onlyTrashed()->where([
+                ['parent_id', '=', $folder_id],
+                ['name', 'Like', '%' . $fileNameSearch . '%'],
+                ['user_id', '=', auth()->id()],
+            ])->get();
+        }
+
+        return view('layouts.home.trash', compact('files'));
+    }
 }
