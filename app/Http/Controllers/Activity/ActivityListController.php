cac3a5d4a880e555ecfab5942195e9290dad6094
DucTran1605 <snowhead2003@gmail.com>
2024-07-08 16:42:35 +0700

    pref: Update count file and folder user have

--- app/Http/Controllers/Activity/ActivityListController.php
+++ app/Http/Controllers/Activity/ActivityListController.php
@@ -2,26 +2,36 @@
 
 namespace App\Http\Controllers\Activity;
 
-use App\Http\Controllers\Controller;
+use App\Models\File;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use Spatie\Activitylog\Models\Activity;
 
 class ActivityListController extends Controller
 {
     /**
-     * List all activity of file that user upload
+     * List all activity of file that user upload, count all file and folder user have
      *
      * @return void
      */
     public function listAllFile()
     {
+        //Show log of file
         $activities = Activity::with(['subject' => function ($query) {
             $query->withTrashed();
         }])->where([
             ['causer_id', '=', auth()->id()],
         ])->orderBy('created_at', 'desc')->paginate(15);
 
-        return view('layouts.home.activity', compact('activities'));
+        // Count all entries with type 'file'
+        $filesCount = File::where('type', '=', 'file')
+            ->count();
+
+        // Count all entries with type 'folder'
+        $foldersCount = File::where('type', '=', 'folder')
+            ->count();
+
+        return view('layouts.home.activity', compact('activities', 'filesCount', 'foldersCount'));
     }
 }
