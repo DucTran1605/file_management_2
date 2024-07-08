c195ac509e308beb87849d93989b7120071ec068
DucTran1605 <snowhead2003@gmail.com>
2024-07-08 09:58:49 +0700

    fix: Fix bug that using / to get in login page

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use App\Http\Controllers\File\FileUploadController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AuthenticatedSessionController::class, 'store']);
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
