e1ae6b6417f4979d7470a86964fbf7ec3a5a847f
DucTran1605 <snowhead2003@gmail.com>
2024-07-15 15:11:25 +0700

    build: Add relation with shared file table

--- app/Models/File.php
+++ app/Models/File.php
@@ -26,21 +26,10 @@ class File extends Model
         'shared_with',
     ];
 
-    /**
-     * Get user who share file
-     */
-    public function user()
+    public function sharedFiles()
     {
-        return $this->belongsTo(User::class);
+        return $this->hasMany(SharedFile::class);
     }
-    /**
-     * Get user that file will shared with
-     */
-    public function sharedWithUser()
-    {
-        return $this->belongsTo(User::class, 'shared_with');
-    }
-
     /**
      * Get the parent folder of the file.
      */
@@ -55,7 +44,6 @@ class File extends Model
     {
         return $this->hasMany(File::class, 'parent_id');
     }
-
     /**
      * Recursively delete all child files and folders.
      */
